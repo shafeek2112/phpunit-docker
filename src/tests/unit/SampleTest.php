f5e7f68a235ffe58317867f9d6204772e3923ce1
Shafeek <mohammedshafeek2112@gmail.com>
2020-09-16 10:13:18 +0800

    Initial commit

---
+++ src/tests/unit/SampleTest.php
@@ -0,0 +1,23 @@
+<?php 
+
+// use PHPUnit\Framework\TestCase;
+
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueAssetsToTrue()
+    {
+        $this->assertTrue(false);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+?>
